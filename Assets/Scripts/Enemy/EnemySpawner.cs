348b5a90d4f32b498f8875c60cec6944ce2992d7
JoJo <jackji2016@hotmail.com>
2023-05-12 01:30:57 -0700

    Enemy Spawner

---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+   [System.Serializable]
+   public class WaveContent
+   {
+      public Transform[] spawnPoints;
+      public GameObject[] enemies;
+   }
+
+   [SerializeField] private WaveContent[] waveContent;
+   [SerializeField] private float waveDelay = 1f;
+
+   [SerializeField] private bool canSpawn = false;
+
+   private float waveTimer = 0f;
+   private int waveCounter = 0;
+
+   public static EnemySpawner Instance { get; private set; }
+
+   private void Start()
+   {
+      Instance = this;
+   }
+
+   private void FixedUpdate()
+   {
+      if(canSpawn) {
+         waveTimer += Time.fixedDeltaTime;
+         if(waveTimer > waveDelay ) {
+            waveTimer = 0f;
+            SpawnWave();
+         }
+      }
+   }
+
+   private void SpawnWave()
+   {
+      if (waveContent.Length == 0) return;
+      var curWave = waveContent[waveCounter];
+      waveCounter++;
+      waveCounter %= waveContent.Length;
+      for( int i = 0; i < curWave.spawnPoints.Length; i++ ) {
+         int j = i % curWave.enemies.Length;
+         Instantiate(curWave.enemies[j], curWave.spawnPoints[i], transform);
+      }
+   }
+
+   public void StartSpawn()
+   {
+      canSpawn = true;
+   }
+
+   public void StopSpawn()
+   {
+      canSpawn = false;
+   }
+}

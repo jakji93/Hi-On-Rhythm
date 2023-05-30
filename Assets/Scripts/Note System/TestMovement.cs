0aa7d334db7ddedbe6f3dba42e17487f525968fe
JoJo <jackji2016@hotmail.com>
2023-05-29 17:23:19 -0700

    charting assist script

--- Assets/Scripts/Note System/TestMovement.cs
+++ Assets/Scripts/Note System/TestMovement.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class TestMovement : MonoBehaviour
 {
-   [SerializeField] float noteSpeed = 3600f;
+   private const float BPM_MULTIPLIER = 480f;
    private float timeSofar = 0f;
+   [SerializeField] private float BPM = 120;
+   [SerializeField] private GameInput playerInputs;
+   private float chartSpeed;
    private void Start()
+   {
+      chartSpeed = BPM / 60 * BPM_MULTIPLIER;
+      Debug.Log(transform.localPosition.x);
+      playerInputs.OnNormal1Pressed += PlayerInputs_OnNormal1Pressed;
+   }
+
+   private void PlayerInputs_OnNormal1Pressed(object sender, System.EventArgs e)
    {
       Debug.Log(transform.localPosition.x);
    }
@@ -14,6 +24,6 @@ public class TestMovement : MonoBehaviour
    private void FixedUpdate()
    {
       timeSofar += Time.fixedDeltaTime;
-      transform.position -= new Vector3(noteSpeed * Time.fixedDeltaTime, 0, 0);
+      transform.position -= new Vector3(chartSpeed * Time.fixedDeltaTime, 0, 0);
    }
 }

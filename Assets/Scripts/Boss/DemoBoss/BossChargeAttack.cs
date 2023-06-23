af1969436d3418bb4a01950c8f8ba52324a25a31
JoJo <jackji2016@hotmail.com>
2023-06-22 20:18:39 -0700

    demo charging attack behavior

---
+++ Assets/Scripts/Boss/DemoBoss/BossChargeAttack.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class BossChargeAttack : Action
+{
+   [SerializeField] private float chargeMoveSpeed;
+   [SerializeField] private int damage;
+   [SerializeField] private float attackSize;
+   [SerializeField] private LayerMask playerMask;
+   [SerializeField] private Transform attackPoint;
+   [SerializeField] private Transform target;
+
+   private bool isHit;
+   private Vector3 direction;
+
+   public override void OnStart()
+   {
+      direction = (target.position - transform.position).normalized;
+      isHit = false;
+   }
+
+   public override TaskStatus OnUpdate()
+   {
+      transform.Translate(chargeMoveSpeed * Time.deltaTime * direction);
+      if(!isHit ) {
+         var collider = Physics2D.OverlapCircle(attackPoint.position, attackSize, playerMask);
+         if (collider != null) {
+            if (collider.gameObject.TryGetComponent(out Health health)) {
+               health.TakeDamage(damage);
+               isHit = true;
+            }
+         }
+      }
+      return TaskStatus.Running;
+   }
+
+   public override void OnDrawGizmos()
+   {
+      Gizmos.color = Color.yellow;
+      Gizmos.DrawWireSphere(attackPoint.position, attackSize);
+   }
+}

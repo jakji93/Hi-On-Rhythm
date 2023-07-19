b4417ae970e7c31e51178d1ba8948f21c7c07167
JoJo <jackji2016@hotmail.com>
2023-07-18 17:51:29 -0700

    osu testing

--- Assets/Scripts/Note System/ChartController.cs
+++ Assets/Scripts/Note System/ChartController.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class ChartController : MonoBehaviour
 {
+   public Transform notehitzone;
+   public Transform hitcircle;
+
    private void Awake()
    {
       Debug.Log(transform.childCount);
+   }
+
+   private void Update()
+   {
+      var distance = transform.position.x - notehitzone.position.x;
+      if(distance < -200 || distance > 1200) hitcircle.gameObject.SetActive(false);
+      else hitcircle.gameObject.SetActive(true);
+      var scale = 1 + distance/480;
+      hitcircle.localScale = new Vector3(scale, scale, 0);
    }
 }

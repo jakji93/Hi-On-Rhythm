94898c3ba09957a17a354827e9879e5ef1958c8e
JoJo <jackji2016@hotmail.com>
2023-07-26 13:58:44 -0700

    tween animation on pause bg

--- Assets/Scripts/UI/PauseGameUI.cs
+++ Assets/Scripts/UI/PauseGameUI.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 using UnityEngine.UI;
 using DG;
 using DG.Tweening;
+using System.Threading.Tasks;
 
 public class PauseGameUI : MonoBehaviour
 {
    [SerializeField] private Button defaultSelectButton;
    [SerializeField] private GameObject pauseGameScreen;
    [SerializeField] private GameObject pauseTab;
+   [SerializeField] private Image pauseBG;
+
+   private float defaultBDAlpha;
 
    private void Start()
    {
       GameplayManager.Instance.OnGamePause += GameplayManager_OnGamePause;
       GameplayManager.Instance.OnGameUnpause += GameplayManager_OnGameUnpause;
+      defaultBDAlpha = pauseBG.color.a;
+      Debug.Log(defaultBDAlpha);
       Hide();
    }
 
@@ -28,8 +34,12 @@ public class PauseGameUI : MonoBehaviour
       Active();
    }
 
-   private void Hide()
+   async private void Hide()
    {
+      var task = new List<Task>();
+      task.Add(pauseTab.transform.DOScale(0, 0.1f).SetUpdate(true).AsyncWaitForCompletion());
+      task.Add(pauseBG.DOFade(0, 0.2f).SetUpdate(true).AsyncWaitForCompletion());
+      await Task.WhenAll(task);
       pauseGameScreen.SetActive(false);
    }
 
@@ -38,6 +48,7 @@ public class PauseGameUI : MonoBehaviour
       pauseGameScreen.SetActive(true);
       defaultSelectButton.Select();
       pauseTab.transform.localScale = Vector3.zero;
+      pauseBG.DOFade(defaultBDAlpha, 0.2f).SetUpdate(true);
       pauseTab.transform.DOScale(1, 0.3f).SetUpdate(true);
    }
 }

c6c5d10ece94af27971ef2a068f0d441b72ec344
JoJo <jackji2016@hotmail.com>
2023-07-31 09:07:40 -0700

    penetration shot

--- Assets/Scripts/Projectile/PenetrationProjectile.cs
+++ Assets/Scripts/Projectile/PenetrationProjectile.cs
@@ -1,25 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using UnityEngine;
 
 public class PenetrationProjectile : Projectile
 {
-
-   private List<GameObject> hitHistory = new List<GameObject>();
-
    override protected void Update()
    {
       Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
       Vector2 newPosition = currentPosition + (Vector2)transform.right * speed * Time.deltaTime;
-      var hitInfos = Physics2D.OverlapCircleAll(attackPoint.position, attackRadius, targetMask);
-      foreach (var hitInfo in hitInfos) {
-         //if (hitHistory.Contains(hitInfo.gameObject)) continue;
-         if (hitInfo.TryGetComponent(out Health health)) {
-            health.TakeDamage(damage * ComboManager.Instance.GetMultiplier());
-            //hitHistory.Append(hitInfo.gameObject);
-         }
-      }
       transform.position = newPosition;
+   }
+
+   private void OnTriggerEnter2D(Collider2D collision)
+   {
+      if (collision.gameObject.CompareTag("Player")) return;
+      if (collision.gameObject.TryGetComponent(out Health health)) {
+         health.TakeDamage(damage * ComboManager.Instance.GetMultiplier());
+      }
    }
 }

d19a3bc06da9a84cc56cf73bdd369176c09032dc
JoJo <jackji2016@hotmail.com>
2023-08-16 21:06:28 -0700

    audio upload

--- Assets/Scripts/UI/LoadingAnimation.cs
+++ Assets/Scripts/UI/LoadingAnimation.cs
@@ -7,12 +7,14 @@ public class LoadingAnimation : MonoBehaviour
 {
    [SerializeField] private Transform left;
    [SerializeField] private Transform right;
+   [SerializeField] private AudioClip sfx;
 
    private void Start()
    {
       left.DOLocalMoveX(0, 1f).SetEase(Ease.InBack);
       right.DOLocalMoveX(0, 1f).SetEase(Ease.InBack).SetDelay(0.3f).OnComplete(() =>
       {
+         ClipPlayer.Instance.PlayClip(sfx);
          transform.DOScale(6, 1f).SetEase(Ease.InOutBack);
       });
    }

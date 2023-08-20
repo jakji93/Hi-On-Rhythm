e45993d77dbf3e944e94e9451ede1e30a7c72862
JoJo <jackji2016@hotmail.com>
2023-08-20 13:50:53 -0700

    partial button control

--- Assets/Scripts/System/SettingsManager.cs
+++ Assets/Scripts/System/SettingsManager.cs
@@ -5,9 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class SettingsManager : MonoBehaviour
 {
+   public static SettingsManager Instance { get; private set; }
+
    [SerializeField] private AudioClip clickClip;
    [SerializeField] private AudioClip buttonClip;
    [SerializeField] private string songSelectionName;
+   [SerializeField] private GameInput gameInput;
+
+   private bool isPanelOpen = false;
+   private PageController pageController;
+
+   private void Awake()
+   {
+      Instance = this;
+   }
+
+   private void Start()
+   {
+      gameInput.OnBackPressed += GameInput_OnBackPressed;
+   }
+
+   private void GameInput_OnBackPressed(object sender, System.EventArgs e)
+   {
+      if (!isPanelOpen) {
+         GoToSongSelect();
+      } else {
+         pageController.ClosePage();
+      }
+   }
 
    public void GoToSongSelect()
    {
@@ -18,5 +43,16 @@ public class SettingsManager : MonoBehaviour
    public void OnButtonClick()
    {
       ClipPlayer.Instance.PlayClip(buttonClip);
+   }
+
+   public void OnBottonEnter()
+   {
+      ClipPlayer.Instance.PlayClip(buttonClip);
+   }
+
+   public void SetPanelStatus(bool panelStatus, PageController controller)
+   {
+      isPanelOpen = panelStatus;
+      pageController = controller;
    }
 }
---
+++ Assets/Scripts/UI/PageController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+
+public class PageController : MonoBehaviour
+{
+   [SerializeField] private RectTransform page;
+   [SerializeField] private CanvasGroup canvasGroup;
+   [SerializeField] private AudioClip openSFX;
+   [SerializeField] private AudioClip closeSFX;
+
+   public void OpenPage()
+   {
+      page.gameObject.SetActive(true);
+      canvasGroup.DOFade(1, 0.2f).SetEase(Ease.Linear);
+      ClipPlayer.Instance.PlayClip(openSFX);
+      SettingsManager.Instance.SetPanelStatus(true, this);
+   }
+
+   public void ClosePage()
+   {
+      ClipPlayer.Instance.PlayClip(closeSFX);
+      canvasGroup.DOFade(0, 0.2f).SetEase(Ease.Linear).OnComplete(() =>
+      {
+         page.gameObject.SetActive(false);
+         SettingsManager.Instance.SetPanelStatus(false, null);
+      });
+   }
+}

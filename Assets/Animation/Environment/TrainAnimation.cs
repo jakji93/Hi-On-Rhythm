0f8ebe259dea689fb0b4c549c6de9edf8663e568
JoJo <jackji2016@hotmail.com>
2023-09-01 10:49:21 -0700

    minor adjustment/testing

---
+++ Assets/Animation/Environment/TrainAnimation.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class TrainAnimation : MonoBehaviour
+{
+   private void Start()
+   {
+      transform.DOMoveX(40f, 20f).SetEase(Ease.Linear).SetLoops(-1, LoopType.Restart);
+   }
+}

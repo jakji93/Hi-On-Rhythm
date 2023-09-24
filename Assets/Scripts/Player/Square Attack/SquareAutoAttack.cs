4b026813b8be477a4393d39a4e47ca9b2672d6a4
JoJo <jackji2016@hotmail.com>
2023-09-23 22:00:11 -0700

    attack visual change

--- Assets/Scripts/Player/Square Attack/SquareAutoAttack.cs
+++ Assets/Scripts/Player/Square Attack/SquareAutoAttack.cs
@@ -9,10 +9,7 @@ public class SquareAutoAttack : MonoBehaviour
    [SerializeField] private Collider2D hitCollider;
    [SerializeField] private ContactFilter2D contactFilter;
    [SerializeField] private int damage;
-   [SerializeField] private SpriteRenderer top;
-   [SerializeField] private SpriteRenderer left;
-   [SerializeField] private SpriteRenderer bottom;
-   [SerializeField] private SpriteRenderer right;
+   [SerializeField] private SpriteRenderer ring;
 
    private List<Collider2D> colliders = new List<Collider2D>();
 
@@ -24,30 +21,20 @@ public class SquareAutoAttack : MonoBehaviour
 
    private void Start()
    {
-      Fade();
       hitCollider.OverlapCollider(contactFilter, colliders);
       foreach (var collider in colliders) {
          if (collider.gameObject.TryGetComponent(out Health health)) {
             health.TakeDamage(damage);
          }
       }
+      FadeRing();
    }
 
-   private async void Fade()
+   private async void FadeRing()
    {
       var tasks = new List<Task>();
-      tasks.Add(top.DOFade(0f, 0.3f).From().SetEase(Ease.Linear).AsyncWaitForCompletion());
-      tasks.Add(left.DOFade(0f, 0.3f).From().SetEase(Ease.Linear).AsyncWaitForCompletion());
-      tasks.Add(right.DOFade(0f, 0.3f).From().SetEase(Ease.Linear).AsyncWaitForCompletion());
-      tasks.Add(bottom.DOFade(0f, 0.3f).From().SetEase(Ease.Linear).AsyncWaitForCompletion());
-      tasks.Add(top.transform.DOScale(Vector3.zero, 0.3f).From().SetEase(Ease.Linear).AsyncWaitForCompletion());
-      tasks.Add(left.transform.DOScale(Vector3.zero, 0.3f).From().SetEase(Ease.Linear).AsyncWaitForCompletion());
-      tasks.Add(right.transform.DOScale(Vector3.zero, 0.3f).From().SetEase(Ease.Linear).AsyncWaitForCompletion());
-      tasks.Add(bottom.transform.DOScale(Vector3.zero, 0.3f).From().SetEase(Ease.Linear).AsyncWaitForCompletion());
-      tasks.Add(top.transform.DOLocalMove(Vector3.zero, 0.3f).From().SetEase(Ease.Linear).AsyncWaitForCompletion());
-      tasks.Add(left.transform.DOLocalMove(Vector3.zero, 0.3f).From().SetEase(Ease.Linear).AsyncWaitForCompletion());
-      tasks.Add(right.transform.DOLocalMove(Vector3.zero, 0.3f).From().SetEase(Ease.Linear).AsyncWaitForCompletion());
-      tasks.Add(bottom.transform.DOLocalMove(Vector3.zero, 0.3f).From().SetEase(Ease.Linear).AsyncWaitForCompletion());
+      tasks.Add(ring.DOFade(0f, 0.3f).SetEase(Ease.Linear).AsyncWaitForCompletion());
+      tasks.Add(ring.transform.DOScale(Vector3.zero, 0.3f).From().SetEase(Ease.Linear).AsyncWaitForCompletion());
       await Task.WhenAll(tasks);
       Destroy(gameObject);
    }
